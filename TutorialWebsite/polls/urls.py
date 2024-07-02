a425c35571e247c033d3d4b1efd2387817805065
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-07-02 12:13:06 +0100

    FInished the first part of the tutorial + made two views in the same app

--- TutorialWebsite/polls/urls.py
+++ TutorialWebsite/polls/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index/", views.index, name="index"),#I think views.index is kind of importing the function index from views, like with numpy.array().
+    path("tableflip/", views.tableflip, name="tableflip"),
 ]
