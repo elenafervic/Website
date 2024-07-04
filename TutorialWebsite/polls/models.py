8454c7cd822f2be90bb68ca3a376b192b002faa6
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-07-04 15:57:06 +0100

    Created and ran migrations corresponding to the addition of two new models to the polls app: Question and Choice.
    I also added the polls app to the installed apps for the "mysite" project.

--- TutorialWebsite/polls/models.py
+++ TutorialWebsite/polls/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-class Question(models.Model):
+class Question(models.Model):#Each of these is a model I think.
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)#Foreign key links each choice to a Question.
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+#I think each "class" (called a model) is a table, and every field is a column
