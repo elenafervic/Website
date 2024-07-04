ee8d0ff30d9064ffc4a4b124950f3f1d2188e23c
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-07-04 12:43:20 +0100

    Commenting out any mention of another view (tableflip), so I can continue with the tutorial and not mess it up

--- TutorialWebsite/polls/urls.py
+++ TutorialWebsite/polls/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index/", views.index, name="index"),#I think views.index is kind of importing the function index from views, like with numpy.array().
-    path("tableflip/", views.tableflip, name="tableflip"),
+    path("", views.index, name="index"),
+    #path("index/", views.index, name="index"),#I think views.index is kind of importing the function index from views, like with numpy.array().
+    #path("tableflip/", views.tableflip, name="tableflip"),
 ]
--- TutorialWebsite/polls/views.py
+++ TutorialWebsite/polls/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
-def tableflip(request):
-    return HttpResponse("(╯°□°)╯︵ ┻━┻")
+#def tableflip(request):
+ #   return HttpResponse("(╯°□°)╯︵ ┻━┻")
 
