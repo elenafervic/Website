9e13692b9902c6e0fa48e0bd77a0797deb3aafb6
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-07-16 12:46:37 +0100

    Made 2 questions, and I added a method in the classes Question and Choice which represents the objects we create by the question or vote names on the database. I also put a method which lets you know if the question was recently created or not.

--- TutorialWebsite/polls/models.py
+++ TutorialWebsite/polls/models.py
@@ -1,13 +1,22 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):#Each of these is a model I think.
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)#Foreign key links each choice to a Question.
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
 
 #I think each "class" (called a model) is a table, and every field is a column
