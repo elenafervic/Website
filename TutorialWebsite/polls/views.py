ee561216fb6e8f35a16cdf146b59565471d3f6fa
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-08-05 16:51:55 +0100

    Introduced render(),  a shortcut for loading a template and returning a HTTP response according to a context.

--- TutorialWebsite/polls/views.py
+++ TutorialWebsite/polls/views.py
@@ -5,11 +5,9 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)#Render returns an HttpResponse object of the given template rendered with the given context.
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 # The format %s tells python you want to replace %s with something else. This something else is the variable after %. 
