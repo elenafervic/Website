3fa615b52effe752c8cc0b723af2f011f4eb8bea
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-08-05 17:11:09 +0100

    Have edited the detail view, now it raises an error if the question we are looking for doesn't exist. I've also linked the detail view to a template which should be executed if the question exists. I've also made a template for the detail view, so now if shows the question and the possible choices.

--- TutorialWebsite/polls/views.py
+++ TutorialWebsite/polls/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
+
 from django.template import loader
+
+
 from .models import Question
 
 def index(request):
@@ -9,11 +13,12 @@ def index(request):
     return render(request, "polls/index.html", context)#Render returns an HttpResponse object of the given template rendered with the given context.
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-# The format %s tells python you want to replace %s with something else. This something else is the variable after %. 
+    question = get_object_or_404(Question, pk=question_id) #It takes in the model question as its first imput.
+    return render(request, "polls/detail.html", {"question": question})
+#I think that it should return the detail page template unless the question doesn't exist.
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
+    response = "You're looking at the results of question %s."# The format %s tells python you want to replace %s with something else. This something else is the variable after %. 
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
