526e0f2b05dbde2ad06e218fe9ca33da78ac2f74
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-08-14 14:22:41 +0100

    Improved commenting

--- TutorialWebsite/polls/views.py
+++ TutorialWebsite/polls/views.py
@@ -9,7 +9,7 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
+    context = {"latest_question_list": latest_question_list}#Dictionary
     return render(request, "polls/index.html", context)#Render returns an HttpResponse object of the given template rendered with the given context.
 
 def detail(request, question_id):
