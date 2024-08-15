50b3d9039ad6e16cab685c306afc3d8d3bdc4ce4
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-08-15 16:24:26 +0100

    Improved the results and vote views, commented on the detail  template and made the results template. Voted a couple of times on the website.

--- TutorialWebsite/polls/views.py
+++ TutorialWebsite/polls/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.http import Http404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from django.template import loader
 
-
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -18,11 +18,33 @@ def detail(request, question_id):
 #I think that it should return the detail page template unless the question doesn't exist.
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."# The format %s tells python you want to replace %s with something else. This something else is the variable after %. 
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])#Returns a string, or a key error (if we didn't pick a choice)
+   
+   #I think the following section is saying "Unless we have a key error, do the "else" stuff".
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",#If you pass this piece of context to the template, it will display the error_message.
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1#Database increases that field by one for the selected choice. F is used instead of other methods because it has the database itself make the change, which avoids race conditions.
+        selected_choice.save()
+        selected_choice.votes=selected_choice.votes+1
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))#Given the name of the view and the variable portion of the url, it returns the full url from the url config (so we don't have to hardcode it)
+#Reverse is what you use in python files that aren't templates to find the URL of a view.
 def tableflip(request):
     return HttpResponse("(╯°□°)╯︵ ┻━┻")
