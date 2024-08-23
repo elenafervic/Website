1769db81f390345c28753ebd4646a73147bb0363
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-08-23 15:36:59 +0100

    Annotated the __str__ method.

--- TutorialWebsite/polls/models.py
+++ TutorialWebsite/polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Question(models.Model):#Each of these is a model I think.
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    def __str__(self):
+    def __str__(self):#Called by doing print(Name_Of_Instance).
         return self.question_text
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
