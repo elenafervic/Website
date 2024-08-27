4c0b9cf4a8ba8bfb4c674459f93ac219f2f3163c
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-08-27 16:58:07 +0100

    I created my own test for a view. It passes and fails when i would expect it to, but I'm not sure if its the way to code this properly.

--- TutorialWebsite/polls/tests.py
+++ TutorialWebsite/polls/tests.py
@@ -4,6 +4,18 @@ from django.utils import timezone
 
 from .models import Question
 
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from django.template import loader
+
+from .models import Choice, Question
+
+from . import views
+
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):#We write the function we are testing, and what we are testing it with.
@@ -17,3 +29,41 @@ class QuestionModelTests(TestCase):
         #I think it creates a test database when testing so no need to delete the question.
 
         #Test methods begin with test
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+class IndexViewTests(TestCase):
+    def test_get_queryset_with_future_question(self):#We write the function we are testing, and what we are testing it with.
+        """
+        get_queryset() should return only questions that have already been published.
+        """
+        Futuretime = timezone.now() + datetime.timedelta(days=30)
+        Pasttime= timezone.now() - datetime.timedelta(days=30)
+        
+        QFuture = Question(question_text="QFuture",pub_date=Futuretime)
+        QFuture.save()
+        
+        QPast=Question(question_text="QPast" ,pub_date=Pasttime)
+        QPast.save()
+        
+        ShouldReturn=Question.objects.filter(pub_date=Pasttime)
+        print(ShouldReturn)
+        
+        self.assertQuerysetEqual(views.IndexView.get_queryset(TestCase), ShouldReturn,transform=lambda x: x)
+        #I think it creates a test database when testing so no need to delete the question.
