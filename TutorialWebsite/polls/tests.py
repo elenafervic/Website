3d3ff80af220cbbf83da2999fa852fa9aecde770
Elena Fernandez-Victorio <elenafervic@gmail.com>
2024-08-27 20:10:37 +0100

    Added a bunch more tests to test the index view. These are from the tutorial.

--- TutorialWebsite/polls/tests.py
+++ TutorialWebsite/polls/tests.py
@@ -8,6 +8,7 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+
 from django.views import generic
 
 from django.template import loader
@@ -47,6 +48,78 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):#This is not a test. Its just a function which I can employ in other tests.
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)#Using the create function means you create and save a new question at the same time.
+
+
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):#Creates no test questions and runs the view to check it does what it's supposed to.
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))#Goes to the url for the index and records the response we receive.
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the
+        index page.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )#This is the appropiate way to compare querysets. Apparently when comparing a model to a context, you need to enclose model with brackets (probably to turn it into a queryset).
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on
+        the index page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question2, question1],
+        )
+    
+
+
 
 class IndexViewTests(TestCase):
     def test_get_queryset_with_future_question(self):#We write the function we are testing, and what we are testing it with.
